89db06a28fbdfe7b12df42c4b16f2922b32347aa
Jona <>
2022-06-13 11:15:23 -0400

    microservices configuration

---
+++ moderation/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://event-bus-srv:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content
+      }
+    });
+  }
+
+  res.send({});
+});
+
+app.post('/events', (req,res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
+})
+
+app.listen(4003, () => {
+  console.log('v4.1 Modearation')
+  console.log('Listening on 4003');
+});
